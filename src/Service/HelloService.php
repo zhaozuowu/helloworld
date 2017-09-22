77637f7e34d1d69578fc93cd25230af5433640b3
zhaozuowu <gaofandi1236677@126.com>
2017-09-22 15:01:19 +0800

    简单的一个开发包

---
+++ src/Service/HelloService.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaozuowu
+ * Date: 17/9/22
+ * Time: 下午2:58
+ */
+
+namespace Stephen\Service;
+
+
+class HelloService
+{
+
+    /**
+     * HelloService constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function index()
+    {
+        return 'Hello World';
+    }
+}
